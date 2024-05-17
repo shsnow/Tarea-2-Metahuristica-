b79feaefce37282a3f29e0227c52a31c2be5db53
Jonas <jonasoviedo574@gmail.com>
2024-05-17 19:29:07 -0400

    codigo hasta hill climbing funcional

---
+++ t2Meta_v1.py
@@ -0,0 +1,231 @@
+import random
+
+#extraer info
+
+def read_file(file_name):
+    with open(file_name, 'r') as file:
+        lines = file.readlines()
+    
+    lines = [line.strip() for line in lines]
+    
+    num_sectors = int(lines[0].split()[-1])
+    num_locations = int(lines[1].split()[-1])
+    installation_cost = list(map(int, lines[2].split()))
+    
+    sectors = []
+    index = 3
+    for _ in range(num_sectors):
+        num_demand_locations = int(lines[index].split()[-1])
+        index += 1
+        demand_locations = list(map(int, lines[index].split()))
+        index += 1
+        sectors.append({
+            "num_demand_locations": num_demand_locations,
+            "demand_locations": demand_locations
+        })
+    
+    return {
+        "num_sectors": num_sectors,
+        "num_locations": num_locations,
+        "installation_cost": installation_cost,
+        "sectors": sectors
+    }
+
+def print_data(data):
+    print(f"Number of sectors (m): {data['num_sectors']}")
+    print(f"Number of locations where a clinic can be installed (n): {data['num_locations']}")
+    print("Installation cost of each clinic:")
+    print(" ".join(map(str, data['installation_cost'])))
+    print()
+
+    for i, sector in enumerate(data['sectors'], start=1):
+        print(f"Sector {i}:")
+        print(f"  Number of locations that satisfy the demand: {sector['num_demand_locations']}")
+        print(f"  Locations: {' '.join(map(str, sector['demand_locations']))}")
+        print()
+
+#----------------------
+
+# parte del greeedy estocastico y determinista
+
+def greedy_deterministic(data):
+    num_locations = data['num_locations']
+    installation_cost = data['installation_cost']
+    sectors = data['sectors']
+    
+    selected_locations = set()
+    covered_sectors = set()
+    
+    print(f"num_locations: {num_locations}")
+    print(f"installation_cost: {installation_cost}")
+    
+    while len(covered_sectors) < data['num_sectors']:
+        best_location = None
+        best_cost_benefit = float('inf')
+        
+        for loc in range(num_locations):
+            if loc in selected_locations:
+                continue
+            benefit = sum(1 for i, sector in enumerate(sectors) if loc in sector['demand_locations'] and i not in covered_sectors)
+            
+            #print(f"Location: {loc}, Benefit: {benefit}")
+            
+            if benefit > 0 and loc < len(installation_cost):
+                cost_benefit = installation_cost[loc] / benefit
+
+                if cost_benefit < best_cost_benefit:
+                    best_cost_benefit = cost_benefit
+                    best_location = loc
+        
+        if best_location is not None:
+            selected_locations.add(best_location)
+            for i, sector in enumerate(sectors):
+                if best_location in sector['demand_locations']:
+                    covered_sectors.add(i)
+        else:
+            print("no hay locaciones validas")
+            break
+    
+    return selected_locations
+
+
+def greedy_stochastic(data):
+    num_locations = data['num_locations']
+    installation_cost = data['installation_cost']
+    sectors = data['sectors']
+    
+    selected_locations = set()
+    covered_sectors = set()
+
+    print(f"num_locations: {num_locations}")
+    print(f"installation_cost: {installation_cost}")
+    
+    while len(covered_sectors) < len(sectors):
+        best_locations = []
+        best_cost_benefit = float('inf')
+        
+        for loc in range(num_locations):
+            if loc in selected_locations:
+                continue
+            benefit = sum(1 for i, sector in enumerate(sectors) if loc in sector['demand_locations'] and i not in covered_sectors)
+            #print(f"Location: {loc}, Benefit: {benefit}")
+            if benefit > 0 and loc < len(installation_cost): 
+                cost_benefit = installation_cost[loc] / benefit
+
+                if cost_benefit < best_cost_benefit:
+                    best_cost_benefit = cost_benefit
+                    best_locations = [loc]
+                elif cost_benefit == best_cost_benefit:
+                    best_locations.append(loc)
+        
+        if best_locations:
+            # Select one of the best locations randomly
+            best_location = random.choice(best_locations)
+            selected_locations.add(best_location)
+            for i, sector in enumerate(sectors):
+                if best_location in sector['demand_locations']:
+                    covered_sectors.add(i)
+        else:
+            print("no hay locaciones validas")
+            break
+    
+    return selected_locations
+
+#--------------------------
+
+# parte del hill climning con la funcion objetivo
+def objective_function(solution, installation_cost):
+    # Ensure all locations in solution are within the valid range
+    if all(0 <= loc < len(installation_cost) for loc in solution):
+        return sum(installation_cost[loc] for loc in solution)
+    else:
+        # Return a very high cost if the solution is invalid
+        return float('inf')
+
+def generate_neighbor(current_solution, num_locations):
+    neighbor = current_solution[:]
+    index = random.randint(0, len(neighbor) - 1)
+    new_loc = random.randint(0, num_locations - 1)
+    neighbor[index] = new_loc
+    return neighbor
+
+def hill_climbing(data, max_iterations=1000):
+    num_locations = data['num_locations']
+    installation_cost = data['installation_cost']
+    initial_solution = greedy_stochastic(data)
+    current_solution = list(initial_solution)
+    current_fitness = objective_function(current_solution, installation_cost)
+    
+    iterations = 0
+    while iterations < max_iterations:
+        neighbor = generate_neighbor(current_solution, num_locations)
+        neighbor_fitness = objective_function(neighbor, installation_cost)
+        
+        if neighbor_fitness < current_fitness:
+            current_solution = neighbor
+            current_fitness = neighbor_fitness
+        else:
+            iterations += 1
+    
+    return current_solution, current_fitness
+
+#------------------------------------------
+
+# su wea del brisket mas complicada que la chucha
+
+def brisket_variant(data, remove_count=2):
+    initial_solution = greedy_stochastic(data)
+    current_solution = list(initial_solution)
+    num_locations = data['num_locations']
+    installation_cost = data['installation_cost']
+    
+    for _ in range(remove_count):
+        if current_solution:
+            current_solution.pop(random.randint(0, len(current_solution) - 1))
+    
+    remaining_sectors = set(data['sectors']) - set(sector for loc in current_solution for sector in data['sectors'] if loc in sector['demand_locations'])
+    while remaining_sectors:
+        best_location = None
+        best_cost_benefit = float('inf')
+        
+        for loc in range(num_locations):
+            if loc in current_solution:
+                continue
+            cost_benefit = installation_cost[loc] / sum(1 for sector in remaining_sectors if loc in sector['demand_locations'])
+            
+            if cost_benefit < best_cost_benefit:
+                best_cost_benefit = cost_benefit
+                best_location = loc
+        
+        if best_location is not None:
+            current_solution.append(best_location)
+            remaining_sectors -= set(sector for sector in remaining_sectors if best_location in sector['demand_locations'])
+    
+    return current_solution, objective_function(current_solution, installation_cost)
+
+#------------------------------------------
+
+file_name = 'C1.txt'
+data = read_file(file_name)
+#print_data(data)
+
+#greedy dterminista (GOOOOD)
+#deterministic_solution = greedy_deterministic(data)
+#print("Greedy Deterministic Solution:", deterministic_solution)
+
+#greedy estocastico (GOOOOD)
+#stochastic_solution = greedy_stochastic(data)
+#print("Greedy Stochastic Solution:", stochastic_solution)
+
+
+#hill climbing (GOOOOOD)
+#best_solution, best_fitness = hill_climbing(data)
+#print("Hill Climbing Best Solution:", best_solution)
+#print("Hill Climbing Best Fitness:", best_fitness)
+
+
+#Brisket
+brisket_solution, brisket_fitness = brisket_variant(data)
+print("Brisket Variant Best Solution:", brisket_solution)
+print("Brisket Variant Best Fitness:", brisket_fitness)
+
